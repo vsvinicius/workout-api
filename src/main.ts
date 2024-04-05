6290eacab11d1a940d4fb6f3de832cbc050b4453
Vinicius da Silva <vinicius.svsilva@gmail.com>
2024-04-04 23:50:56 -0300

    solves cors issue

--- src/main.ts
+++ src/main.ts
@@ -7,7 +7,7 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   app.useGlobalPipes(new ValidationPipe());
-  app.enableCors({ origin: '*' });
+  app.enableCors({ origin: '*', allowedHeaders: '*' });
 
   await app.listen(env.port);
 }
